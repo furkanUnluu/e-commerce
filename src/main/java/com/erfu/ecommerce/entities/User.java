2f4c4951d505bf94cf34f863531c16ef42c86d92
Eren Aydın <83093241+erenaydn@users.noreply.github.com>
2022-02-13 00:25:37 +0300

    add

---
+++ src/main/java/com/erfu/ecommerce/entities/User.java
@@ -0,0 +1,15 @@
+package com.erfu.ecommerce.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class User {
+
+    private Long id;
+
+
+}
